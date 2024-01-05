d8c1ffd28e33bab6a27cf0b85a6d1b536ae1556f
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-01-05 16:10:39 +0300

    add WeekType Model

---
+++ django-studenda/studenda/WeekType/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class WeekType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
